270002b7b2ccafb0f8849f3239203841c7c53e04
GDHenriquez <gregorioduranedrian123@gmail.com>
2024-05-23 17:02:07 -0400

    GDuran form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,14 +6,51 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
 
   formcontact: FormGroup;
+  typeDni: string = 'DNI'
+  /* userActive: string = 'Pedro' */
+
+  /* infoUser: any = {
+    name: 'Pedro',
+    lastName: 'Azoka',
+    dni: 123456
+  } */
+
+  
+  //ngOnInit(): void {
+   /*  this.formcontact.get('name')?.setValue(this.userActive); */
+   //this.formcontact.get('lastName')?.clearValidators()
+   //this.formcontact.get('lastName')?.updateValueAndValidity()
+   //this.formcontact.patchValue({
+    //name: this.infoUser.name,
+    /* lastName: this.infoUser.lastName, */
+    //dni: this.infoUser.dni
+   //})
+    //this.formcontact.get('name')?.disable();
+    //this.formcontact.get('lastName')?.disable();
+    //this.formcontact.get('dni')?.disable();
+  //}
+
+  //subcribe to form, listen to all form
+  ngOnInit(): void {
+    /* this.formcontact.valueChanges.subscribe(valor => {
+      console.log(valor)
+    }) */
+    this.formcontact.get('typeDni')?.valueChanges.subscribe(value => {
+      this.typeDni = value
+      console.log(value)
+    })
+  }
 
   constructor(private form : FormBuilder){
     this.formcontact = this.form.group({
-      name: ['', [Validators.required, Validators.minLength(3)]],  
-      email: ['',[Validators.required, Validators.email]]
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.minLength(3)]],
+      dni: ['', [Validators.required]],  
+      email: ['',[Validators.required, Validators.email]],
+      typeDni: ['DNI']
     })
   }
 
